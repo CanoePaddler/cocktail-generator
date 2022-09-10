6d135793604969b7f1d676c87f65a181a2c95c35
Will Vining <awvining91@gmail.com>
2022-09-09 19:37:31 -0700

    added comments to html and javascript

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
+// Here is the logic for the Giphy API
 
-
+//This function grabs the input value from the search bar
 function giphyApi() {
     var getInfo = document.getElementById("user-input").value
     console.log(getInfo)
-    
+    //Next we send a request to Giphy's server for the info we want
     var myApi = "4SGZMcOUobdN4nX6cFWdyX3pDbRi5Akr"
     var apiAddress = `https://api.giphy.com/v1/gifs/search?q=${getInfo}&rating=g&api_key=${myApi}`
-    
+    //Then we get the data lump back and use json to turn into something we can use
     fetch(apiAddress).then(function(data) {
         return data.json()
     })
     .then(function(json){
       console.log(json.data[0].images.fixed_height.url)
       var giphyLocation = json.data[0].images.fixed_height.url
-
+    //Next we grab the specific info we want from the fetch, which is the gif
       var img = document.createElement("img")
       img.setAttribute("src", giphyLocation)
       document.body.appendChild(img)
+      //finally we create the gif on the html with methods :)
     })
   }
 
@@ -29,6 +31,6 @@ function giphyApi() {
   https://www.youtube.com/watch?v=mj8_w11MvH8&list=WL&index=15
 
 
-
+  Will V worked on the Giphy API :)
 
   */
