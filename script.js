80df721f2fa5c4b20327fc821219669e15218dcb
Will Vining <awvining91@gmail.com>
2022-09-09 19:05:14 -0700

    added some references and worked on variables

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-prompt('testing');
 
-function sendApiRequest() {
-    var userInput = document.getElementById("input").value
-    console.log(userInput)
+
+function giphyApi() {
+    var getInfo = document.getElementById("input").value
+    console.log(getInfo)
     
-    var giphyApiKey = "4SGZMcOUobdN4nX6cFWdyX3pDbRi5Akr"
-    var giphyApiURL = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=g&api_key=${giphyApiKey}`
+    var myApi = "4SGZMcOUobdN4nX6cFWdyX3pDbRi5Akr"
+    var giphyApiURL = `https://api.giphy.com/v1/gifs/search?q=${getInfo}&rating=g&api_key=${myApi}`
     
     fetch(giphyApiURL).then(function(data) {
         return data.json()
@@ -18,3 +18,16 @@ function sendApiRequest() {
       document.body.appendChild(img)
     })
   }
+
+  /* References:
+
+  https://www.youtube.com/watch?v=R59U6QaOEFg&list=WL&index=17&t=1130s
+  https://developers.giphy.com/dashboard/?
+  https://developers.giphy.com/docs/api/
+  https://www.youtube.com/watch?v=HRh6zHRwRLo&list=WL&index=16&t=5s
+  https://www.youtube.com/watch?v=mj8_w11MvH8&list=WL&index=15
+
+
+
+
+  */
