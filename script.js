cced2e9a2ce9deaa320d4ddfda5a440582068395
Will Vining <awvining91@gmail.com>
2022-09-14 16:35:57 -0700

    more local storage work

--- script.js
+++ script.js
@@ -37,56 +37,62 @@ function giphyApi() {
 
   //----------------------------------------//
 
-  /*   */
+  /* Here I use some methods to grab the form ids in the html and save them as 
+  variables*/
 
-  var fullname = document.getElementById('yourname'),
-    address = document.getElementById('email'),
+  var yourname = document.getElementById('yourname'),
+
+    email = document.getElementById('email'),
     saveIt = document.getElementById('save'),
 
     loadIt = document.getElementById('load'),
     clearIt = document.getElementById('clear'),
 
     resetIt = document.getElementById('reset');
-
-var localStore = {
+//Next I created an object called saveLcal with a number of functions to help save
+//the user inputs for their name and email address
+var saveLocal = {
+  //save function
   saveLocalStorage: function() {
-    localStorage.setItem('item', JSON.stringify(this.getInputValue()));
+    localStorage.setItem('item', JSON.stringify(this.grabInfo()));
   },
-
+  //load function
   loadLocalStorage: function() {
-    var store = JSON.parse(localStorage.getItem('item'));
-    if ( store ) {
-      fullname.value = store.fullname;
-      address.value = store.address;
+    var savestuff = JSON.parse(localStorage.getItem('item'));
+    if ( savestuff ) {
+      yourname.value = savestuff.yourname;
+      email.value = savestuff.email;
     } 
   },
+  //clear function
   clearLocalStorage: function() {
     localStorage.removeItem('item');
   },
-  getInputValue: function() {
+  grabInfo: function() {
     return {
-      fullname: fullname.value,
-      address: address.value
+      yourname: yourname.value,
+      email: email.value
+ //The functions operate based on whether the use pushes the save, load, or clear button
     }
   }
 };
 
-
+//This part adds an event listener to the buttons to call the functions to clear, save, load or reset info
 saveIt.addEventListener('click', function() {
-  localStore.saveLocalStorage();
+  saveLocal.saveLocalStorage();
 }, false);
 
 loadIt.addEventListener('click', function() {
-  localStore.loadLocalStorage();
+  saveLocal.loadLocalStorage();
 }, false);
 
 clearIt.addEventListener('click', function() {
-  localStore.clearLocalStorage();
+  saveLocal.clearLocalStorage();
 }, false);
 
 resetIt.addEventListener('click', function() {
-  fullname.value = '';
-  address.value = '';
+  yourname.value = '';
+  email.value = '';
 }, false);
 
 /* References
